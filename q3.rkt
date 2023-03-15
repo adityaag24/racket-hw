63bf6eccd5fc42291e36e6dd82c388239907b03a
Aditya Agrawal <aditya@Aditya.localdomain>
2023-03-16 00:51:02 +0530

    Edits

--- q3.rkt
+++ q3.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(define lst1 (cons (cons (list 1 2) '()) (list 4)))
+(define lst1 (cons (cons (cons 1 (cons 2 '())) '()) (cons 4 '())))
 (define lst2 (cons 1 (cons (cons 2 (cons (list 3) (list 4))) '())))
 (define lst3 (cons 2 (cons (cons 4 (cons 5 '())) (cons 6 '()))))
 (define lst4 (cons 5 (cons (cons (cons 4 '()) '()) (cons 3 '()))))
